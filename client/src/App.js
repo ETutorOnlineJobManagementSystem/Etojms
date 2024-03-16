35cee9589a293b735104a428bc752982d87f4f9e
sowjanya1308 <sowjanyasankara@gmail.com>
2024-03-16 15:30:06 +0530

    login and register

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {  Route, Routes, Navigate  } from "react-router-dom";
 import StartPage from "./screens/startpage/start";
 import Login from "./screens/login/login";
 import './App.css';
+import Register from "./screens/register/register";
 const App = () => {
   return (
     <>
@@ -12,7 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/" Component={StartPage}/>
         <Route path="*" element={<Navigate replace to={"/"} />} />       
-        <Route path="/login" Component={Login}/>
+        <Route path="/login" Component={Login} />
+        <Route path="/register" Component={Register} />
       </Routes>
     </>
   );
